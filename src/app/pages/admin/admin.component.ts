7e4b442aa0b6d96e159af377cee2e515d86b86a1
luccabibar <luccabibar@gmail.com>
2023-11-28 03:04:59 -0300

    tela de admin agora expulsa usuarios

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { RelatorioService } from './relatorioService/relatorio.service';
 import { Observable } from 'rxjs';
+import { PerfilService } from '../perfil/perfilService/perfil.service';
+import { Router } from '@angular/router';
+import { UserService } from 'src/shared/services/userService/user.service';
+import { Usuario } from 'src/assets/classes/usuario';
 
 @Component({
   selector: 'app-admin',
@@ -10,8 +14,35 @@ import { Observable } from 'rxjs';
 export class AdminComponent {
 
   constructor (
-    public relatorioSrv: RelatorioService
-  ) { }
+		public userSrv: UserService,
+    public relatorioSrv: RelatorioService,
+    public perfilSrv: PerfilService,
+    public router: Router,
+  ) { 
+
+    let user = userSrv.getLoggedUser();
+    
+    if(user){
+      perfilSrv.getUserData(user.cpf).subscribe(
+        (response) => {
+          let userData = response as unknown as Usuario;
+          console.log(userData);
+
+          if(!userData.admin){
+            this.returnToLoja();
+          }
+        },
+        (error) => {
+          // TODO: error
+          console.log(error);
+          this.returnToLoja();
+        }
+      );
+    }
+    else{
+      this.returnToLoja();
+    }
+  }
 
   gerarRelatorio(tipoRelatorio: String)
   {
@@ -40,8 +71,14 @@ export class AdminComponent {
         console.log(result);
       },
       (error) => {
+        // TODO: msg erro
         console.log(error);
       }
     )
+  }
+
+  returnToLoja(){
+    // TODO: msg erro
+		this.router.navigate(['/loja']);
   }
 }
